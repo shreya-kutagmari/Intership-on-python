2a01b5dd539f3561afd9d67bb6a9ee420b2dd025
shreya-kutagmari <167098978+shreya-kutagmari@users.noreply.github.com>
2024-04-25 18:57:17 +0530

    Add files via upload

---
+++ pattern8.py
@@ -0,0 +1,5 @@
+rows = int(input("Enter the number of rows: "))  
+for num in range(rows+1):    
+        for i in range(num):  
+            print(num, end=" ")       
+        print()
---
+++ pattern9.py
@@ -0,0 +1,6 @@
+rows = int(input("Enter the number of rows: "))
+for i in range(rows, 0, -1):  
+    num = i
+    for j in range(0, i):  
+            print(num, end=" ")       
+    print()
